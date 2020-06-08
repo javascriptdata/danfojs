b6a70c3d197ee85139a43e854ee6d59f9863039b
steveoni <steohenoni2@gmail.com>
2020-06-09 00:34:50 +0100

    add dummy method to frame.js

--- src/core/frame.js
+++ src/core/frame.js
@@ -1,6 +1,31 @@
 export class DataFrame {
 
-    print(val) {
-        console.log(`Printing In DataFrame ${val}`);
+    drop(col){}
+
+    loc(row_list,colum_list){}
+
+    iloc(row_indexes, colum_indexes){}
+
+
+
+    addColum(col,value){}
+
+    isnan(){}
+
+    nanIndex(){}
+
+    static groupby(col1,col2,aggregate){
+
     }
+
+    static concatenate(df1,df2,axis){}
+
+    static query(column, operator,value){}
+
+    static dummy(series){}
+    
+    static merge(){}
+
+    
+
 }
