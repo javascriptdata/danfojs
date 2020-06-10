427a4551d7831c848f8e8dc9930e03c1ba343909
Rising <risingodegua@gmail.com>
2020-06-10 22:06:41 +0100

    Add tests for utils

--- danfojs/src/core/utils.js
+++ danfojs/src/core/utils.js
@@ -19,28 +19,23 @@ export class Utils {
         return typeof value === 'number' && isFinite(value);
     }
 
-    // Returns if a value is an array
-    isArray(value) {
-        return value && typeof value === 'object' && value.constructor === Array;
-    }
-
     // Returns if a value is an object
     isObject(value) {
         return value && typeof value === 'object' && value.constructor === Object;
     }
 
-    // Returns if a value is null
-    isNull(value) {
-        return value === null;
-    }
+    // // Returns if a value is null
+    // isNull(value) {
+    //     return value === null;
+    // }
 
-    // Returns if a value is undefined
-    isUndefined(value) {
-        return typeof value === 'undefined';
-    }
+    // // Returns if a value is undefined
+    // isUndefined(value) {
+    //     return typeof value === 'undefined';
+    // }
 
-    // Returns if a value is a boolean
-    isBoolean(value) {
-        return typeof value === 'boolean';
-    }
+    // // Returns if a value is a boolean
+    // isBoolean(value) {
+    //     return typeof value === 'boolean';
+    // }
 }
--- danfojs/tests/core/utils.js
+++ danfojs/tests/core/utils.js
@@ -1,4 +1,25 @@
-// import { assert } from "chai";
-// import {Utils} from '../../src/core/utils';
-// const utils = new Utils
+import { assert } from "chai";
+import { Utils } from '../../src/core/utils';
+const utils = new Utils
 
+
+describe("Utils Functions", function () {
+    it("removes an element from an array", function () {
+        let arr = [1, 2, 3, 4]
+        assert.deepEqual(utils.remove(arr, 2), [1, 2, 4])
+    })
+    it("Checks if variable is a string", function () {
+        let arr = ["1", "2"]
+        assert.isTrue(utils.isString(arr[0]))
+    })
+    it("Checks if variable is a number", function () {
+        let arr = [1, 2, 3, 4]
+        assert.isTrue(utils.isNumber(arr[0]))
+    })
+    it("Checks if variable is an object", function () {
+        let arr = [{ "name": "John", "age": 28 },
+                    { "name": "Mark", "age": 12 }]
+        assert.isTrue(utils.isObject(arr[0]))
+    })
+
+})
