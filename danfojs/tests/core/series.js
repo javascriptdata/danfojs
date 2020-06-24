966264ea9bb3d0dcb9799bfd6c8734516c581421
Rising <risingodegua@gmail.com>
2020-06-25 00:41:17 +0100

    add subtraction operation to Series

--- danfojs/tests/core/series.js
+++ danfojs/tests/core/series.js
@@ -89,6 +89,34 @@ describe("Series", function () {
 
     })
 
+    describe("sub", function () {
+        it("Return Subtraction of series with another series", function () {
+            let data1 = [30, 40, 39, 1, 2, 1]
+            let data2 = [1, 2, 3, 4, 5, 6]
+            let sf1 = new Series(data1)
+            let sf2 = new Series(data2)
+            assert.deepEqual(sf1.sub(sf2).values, [29, 38, 36, -3, -3, -1])
+        })
+        it("Return Subtraction of series with a single value (Broadcasting)", function () {
+            let data = [1, 2, 3, 4, 5]
+            let sf = new Series(data)
+            assert.deepEqual(sf.sub(1).values, [0, 1, 2, 3, 4])
+        })
+        it("Throws type error on Subtraction of string type", function () {
+            let data = [1, 2, 3, 4]
+            let data2 = ["A", "B", "C", "d"]
+            let sf = new Series(data)
+            let sf2 = new Series(data2)
+            assert.throws(() => { sf.sub(sf2) }, Error, "Argument 'x' passed to 'cast' must be numeric tensor, but got string tensor")
+        })
+        it("Throws length error if series lenght mixmatch", function () {
+            let data = [1, 2, 3, 4]
+            let data2 = [1, 2, 3, 4, 5, 6]
+            let sf = new Series(data)
+            let sf2 = new Series(data2)
+            assert.throws(() => { sf.sub(sf2) }, Error, " Incompatible shapes: [4] vs. [6]")
+        })
 
+    })
 
 })
