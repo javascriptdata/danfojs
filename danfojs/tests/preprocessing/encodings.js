bcf34be704b5f09382a6137c366eec02d67680ab
steveoni <steohenoni2@gmail.com>
2020-08-02 16:12:05 +0100

    implement onehotencoder

--- danfojs/tests/preprocessing/encodings.js
+++ danfojs/tests/preprocessing/encodings.js
@@ -1,11 +1,12 @@
 import { assert } from "chai"
-import { LabelEncoder } from "../../src/preprocessing/encodings"
+import { LabelEncoder, OneHotEncoder } from "../../src/preprocessing/encodings"
+import { Series } from "../../src/core/series"
 
 describe("Encodings", function(){
 
     describe("LabelEncoder", function(){
 
-        it("basic test", function(){
+        it("test the label encoding on array", function(){
             let data = ["dog","cat","man","dog","cat","man","man","cat"]
             let encode = new LabelEncoder()
             let fit_data = [
@@ -15,7 +16,60 @@ describe("Encodings", function(){
             assert.deepEqual(encode.fit(data),fit_data)
             assert.deepEqual(encode.transform(["dog","man"]),[0,2])
         });
+        it("test the label encoding on Series", function(){
+            let data = ["dog","cat","man","dog","cat","man","man","cat"]
+            let series = new Series(data)
+            let encode = new LabelEncoder()
+            let fit_data = [
+                0, 1, 2, 0,
+                1, 2, 2, 1
+              ]
+            assert.deepEqual(encode.fit(series),fit_data)
+            assert.deepEqual(encode.transform(["dog","man"]),[0,2])
+        });
+
     })
+
+    describe("OneHotEncoder", function(){
+
+        it("test onehotencoding on array", function(){
+            let data = ["dog","cat","man","dog","cat","man","man","cat"]
+            let encode = new OneHotEncoder()
+            let fit_data = [
+                [ 1, 0, 0 ],
+                [ 0, 1, 0 ],
+                [ 0, 0, 1 ],
+                [ 1, 0, 0 ],
+                [ 0, 1, 0 ],
+                [ 0, 0, 1 ],
+                [ 0, 0, 1 ],
+                [ 0, 1, 0 ]
+              ]
+            let transform_data = [ [ 0, 0, 1 ], [ 0, 1, 0 ] ]
+
+            assert.deepEqual(encode.fit(data).values, fit_data);
+            assert.deepEqual(encode.transform(["man","cat"]).values, transform_data)
+        })
+        it("test onehotencoding on Series", function(){
+            let data = ["dog","cat","man","dog","cat","man","man","cat"]
+            let series = new Series(data)
+            let encode = new OneHotEncoder()
+            let fit_data = [
+                [ 1, 0, 0 ],
+                [ 0, 1, 0 ],
+                [ 0, 0, 1 ],
+                [ 1, 0, 0 ],
+                [ 0, 1, 0 ],
+                [ 0, 0, 1 ],
+                [ 0, 0, 1 ],
+                [ 0, 1, 0 ]
+              ]
+            let transform_data = [ [ 0, 0, 1 ], [ 0, 1, 0 ] ]
+
+            assert.deepEqual(encode.fit(series).values, fit_data);
+            assert.deepEqual(encode.transform(["man","cat"]).values, transform_data)
+        })
+    });
 
 
 });
