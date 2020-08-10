c562bbeb1fdd610f23256e1fcdcb68a1081d0015
Rising Odegua <risingodegua@gmail.com>
2020-08-10 01:47:19 +0100

    build for web

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: './dist/index.js',
     output: {
         path: path.resolve(__dirname, "dist"),
+        filename: 'index.js',
         library: 'dfd',
     },
     module: {
