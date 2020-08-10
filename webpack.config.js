be9d0fdbcbc758bb9340f8569434f0baa96dee2b
Rising Odegua <risingodegua@gmail.com>
2020-08-10 01:55:38 +0100

    fix webpack build file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dist/index.js',
+    entry: './danfojs/src/index.js',
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'index.js',
