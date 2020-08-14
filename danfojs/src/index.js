13823e13e1fe935a9b0a660f9697e4cd883119b1
CREATIVE JOE <52638039+creativeJoe007@users.noreply.github.com>
2020-08-14 01:49:55 +0100

    Prettier fix

--- danfojs/src/index.js
+++ danfojs/src/index.js
@@ -2,11 +2,11 @@
 export { Series } from './core/series';
 export { DataFrame } from './core/frame';
 export { to_datetime } from './core/timeseries';
-export {read_csv} from './io/reader'
+export { read_csv } from './io/reader';
 // export {read_json} from './io/reader'
-export {merge} from './core/merge'
-export {concat} from './core/concat'
-export {LabelEncoder, OneHotEncoder} from './preprocessing/encodings' //TODO Create js file to expose functions
-export {MinMaxScaler, StandardScaler} from './preprocessing/scalers'
-export {date_range} from './core/date_range'
-export {get_dummies} from './core/get_dummies'
+export { merge } from './core/merge';
+export { concat } from './core/concat';
+export { LabelEncoder, OneHotEncoder } from './preprocessing/encodings'; //TODO Create js file to expose functions
+export { MinMaxScaler, StandardScaler } from './preprocessing/scalers';
+export { date_range } from './core/date_range';
+export { get_dummies } from './core/get_dummies';
