c74d7b494ece19d1031632ce25b1b7d79ca0b005
Aditya <adityazope261@gmail.com>
2020-09-20 13:45:59 +0530

    Add feature to read excel files from local file / remote url

--- danfojs/src/index.js
+++ danfojs/src/index.js
@@ -2,7 +2,7 @@
 export { Series } from './core/series';
 export { DataFrame } from './core/frame';
 export { to_datetime } from './core/timeseries';
-export {read_csv, read_json} from './io/reader'
+export {read_csv, read_json, read_excel} from './io/reader'
 // export {read_json} from './io/reader'
 export {merge} from './core/merge'
 export {concat} from './core/concat'
--- danfojs/src/io/reader.js
+++ danfojs/src/io/reader.js
@@ -7,6 +7,7 @@ import { Utils } from '../core/utils'
 import fs from 'fs'
 import fetch from 'node-fetch'
 
+import XLSX from 'xlsx';
 const utils = new Utils()
 
 
@@ -69,21 +70,43 @@ export const read_json = async (source) => {
 }
 
 
-// /**
-//  * Reads a Excel file from local or remote address
-//  * 
-//  * @param {source} URL or local file path to retreive JSON file.
-//  * @returns {Promise} DataFrame structure of parsed CSV data
-//  */
-// export const read_excel = async (source) => {
+/**
+ * Reads an Excel file from local or remote address
+ *
+ * @param {source} URL or local file path to retreive Excel file.
+ * @param {sheetName} (Optional) Name of the sheet which u want to parse. Default will be the first sheet.
+ * @description Assume following format: First non empty row represents column and next rows represents data until the next empty row.
+ * @returns {Promise} DataFrame structure of parsed Excel data
+*/
+export const read_excel = async (source, sheetName) => {
+    var isAUrl = source.match(/(http(s?)):\/\//g);
+    var workbook;
+    try {
+        if (utils.__is_node_env()) {
+            // inside Node Env
+            if (isAUrl) {
+                let res = await fetch(source, { method: "Get" })
+                res = await res.arrayBuffer();
+                res = new Uint8Array(res);
+                workbook = XLSX.read(res, {type:"array"});
+            }
+        }
+        if(!isAUrl) {
+            workbook = XLSX.readFile(source);
+        }
+        // convert from workbook to array of arrays
+        const worksheet = workbook.Sheets[sheetName || workbook.SheetNames[0]];
 
-//     xlsxFile('./Data.xlsx').then((rows) => {
-//         console.log(rows);
-//         console.table(rows);
-//        })
-// }
-
-
+        //sheet_to_json returns array of arrays
+        var data = XLSX.utils.sheet_to_json(worksheet, {header:1});
+        const columnNames = data[0];
+        data = [].concat(data.slice(1));
+        let df = new DataFrame(data, {columns : columnNames});
+        return df;
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // /**
 //  * Reads a SQL Database into DataFrame
