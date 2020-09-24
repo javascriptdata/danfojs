5972e3019a48b749e86c37f9a38000e44133cbbd
Rising Odegua <risingodegua@gmail.com>
2020-09-24 12:23:32 +0100

    add support to read CSV locally

--- danfojs/src/io/reader.js
+++ danfojs/src/io/reader.js
@@ -23,12 +23,18 @@ const utils = new Utils()
  * @returns {Promise} DataFrame structure of parsed CSV data
  */
 export const read_csv = async (source, chunk) => {
+    if (!(utils.__is_browser_env() || source.startsWith("file://") || source.startsWith("http"))) {
+        //probabily a relative path, append file:// to it
+        source = `file://${process.cwd()}/${source}`
+    } 
+
     let data = []
     const csvDataset = tf.data.csv(source)
     const column_names = await csvDataset.columnNames()
     const sample = await csvDataset.take(chunk)
     await sample.forEachAsync(row => data.push(Object.values(row)))
     let df = new DataFrame(data, { columns: column_names })
+    df.head().print()
     return df
 }
 
@@ -59,7 +65,7 @@ export const read_json = async (source) => {
             })
         }
     } else {
-        
+
         let res = await fetch(source, { method: "Get" })
         let json = await res.json()
         let df = new DataFrame(json)
@@ -82,7 +88,7 @@ export const read_json = async (source) => {
  * @returns {Promise} DataFrame structure of parsed Excel data
 */
 export const read_excel = async (kwargs) => {
-    var {source, sheet_name, header_index, data_index} = kwargs;
+    var { source, sheet_name, header_index, data_index } = kwargs;
     var is_a_url = source.match(/(http(s?)):\/\//g);
     var workbook;
     if (!header_index) {
@@ -100,42 +106,42 @@ export const read_excel = async (kwargs) => {
                 let res = await fetch(source, { method: "Get" })
                 res = await res.arrayBuffer();
                 res = new Uint8Array(res);
-                workbook = XLSX.read(res, {type:"array"});
+                workbook = XLSX.read(res, { type: "array" });
             }
         }
-        if(!is_a_url) {
+        if (!is_a_url) {
             workbook = XLSX.readFile(source);
         }
         // Parse worksheet from workbook
         const worksheet = workbook.Sheets[sheet_name || workbook.SheetNames[0]];
         var range = XLSX.utils.decode_range(worksheet['!ref']);
         var column_names = [], data = [];
-        for(var R = header_index-1; R <= range.e.r; ++R) {
+        for (var R = header_index - 1; R <= range.e.r; ++R) {
             var row_data = [];
-            for(var C = range.s.c; C <= range.e.c; ++C) {
+            for (var C = range.s.c; C <= range.e.c; ++C) {
                 var cell_ref;
 
                 //Populate column_names
                 if (R == header_index - 1) {
-                    cell_ref = XLSX.utils.encode_cell({c:C, r:header_index - 1});
-                    if(worksheet[cell_ref]) {
+                    cell_ref = XLSX.utils.encode_cell({ c: C, r: header_index - 1 });
+                    if (worksheet[cell_ref]) {
                         column_names.push(worksheet[cell_ref].v);
                     }
                 }
 
                 //Populate corresponding data row
-                if (R >= data_index-1) {
-                    cell_ref = XLSX.utils.encode_cell({c:C, r:R});
-                    if(worksheet[cell_ref]) {
+                if (R >= data_index - 1) {
+                    cell_ref = XLSX.utils.encode_cell({ c: C, r: R });
+                    if (worksheet[cell_ref]) {
                         row_data.push(worksheet[cell_ref].v);
                     }
                 }
             }
-            if (R >= data_index-1) {
+            if (R >= data_index - 1) {
                 data.push(row_data);
             }
         }
-        let df = new DataFrame(data, {columns : column_names});
+        let df = new DataFrame(data, { columns: column_names });
         return df;
     } catch (err) {
         console.error(err);
--- danfojs/tests/io/reader.js
+++ danfojs/tests/io/reader.js
@@ -2,68 +2,66 @@ import { assert } from "chai";
 import { read_csv, read_json, read_excel } from '../../src/io/reader';
 
 
-// describe("read_csv", async function () {
+describe("read_csv", async function () {
 
-//     it("reads a csv file from source over the internet", async function () {
-//         const csvUrl =
-//             'https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv';
+    it("reads a csv file from source over the internet", async function () {
+        const csvUrl =
+            'https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv';
 
-//         const df = await read_csv(csvUrl, {}, 10)
-//         const num_of_columns = (df.column_names).length
-//         assert.equal(num_of_columns, 13)
-//     })
+        const df = await read_csv(csvUrl, {}, 10)
+        const num_of_columns = (df.column_names).length
+        assert.equal(num_of_columns, 13)
+    })
 
-//     it("reads a csv file from source from local disk", async function () {
-//         const csvUrl =
-//             'file:///home/dsn/personal/opensource/danfojs/danfojs/data/all_brand_df.csv';
+    it("reads a csv file from source from local disk", async function () {
+        const csvUrl = 'danfojs/tests/samples/titanic.csv';
 
-//         read_csv(csvUrl).then((df) => {
-//             const num_of_columns = (df.column_names).length
-//             console.log(df.head() + "")
-//             assert.equal(num_of_columns, 12)
-//         })
+        read_csv(csvUrl).then((df) => {
+            const num_of_columns = (df.column_names).length
+            assert.equal(num_of_columns, 8)
+        })
 
-//     })
-// })
+    })
+})
 
 
 
-// describe("read_json", async function () {
+describe("read_json", async function () {
 
-//     it("reads a json file from source over the internet", async function () {
-//         const jUrl =
-//             'https://raw.githubusercontent.com/risenW/Tensorflowjs_Projects/master/recommender-sys/Python-Model/web_book_data.json';
+    it("reads a json file from source over the internet", async function () {
+        const jUrl =
+            'https://raw.githubusercontent.com/risenW/Tensorflowjs_Projects/master/recommender-sys/Python-Model/web_book_data.json';
 
-//         read_json(jUrl).then((df) => {
-//             const num_of_columns = df.columns.length
-//             assert.equal(num_of_columns, 4)
-//         })
-//     })
+        read_json(jUrl).then((df) => {
+            const num_of_columns = df.columns.length
+            assert.equal(num_of_columns, 4)
+        })
+    })
 
-//     it("reads a json file from source from local disk", async function () {
-//         const jUrl =
-//             '/Users/mac/Documents/Tensorflowjs_Projects-master/recommender-sys/Python-Model/web_book_data.json';
+    // it("reads a json file from source from local disk", async function () {
+    //     const jUrl = 'danfojs/tests/samples/book.json';
 
-//         read_json(jUrl).then((df) => {
-//             const num_of_columns = (df.column_names).length
-//             console.log(df.head() + "")
-//             assert.equal(num_of_columns, 4)
-//         })
+    //     read_json(jUrl).then((df) => {
+    //         const num_of_columns = (df.column_names).length
+    //         assert.equal(num_of_columns, 4)
+    //     })
 
-//     })
-// })
+    // })
+})
 
 describe("read_excel", async function () {
 
     it("reads an excel file from source over the internet", async function () {
         const remote_url = 'https://file-examples-com.github.io/uploads/2017/02/file_example_XLS_100.xls';
-        const df = await read_excel({source : remote_url});
-        assert(df.columns.length, 8);
+        read_excel({source : remote_url}).then(df=>{
+            assert(df.columns.length, 8);
+        })
     })
 
     it("reads an excel file from source from local disk", async function () {
         const file_url = 'danfojs/tests/samples/SampleData.xlsx';
-        const df = await read_excel({source : file_url, header_index : 7, data_index : 8});
-        assert(df.columns.length, 4);
+        read_excel({source : file_url, header_index : 7, data_index : 8}).then(df=>{
+            assert(df.columns.length, 4);
+        })
     })
 })
