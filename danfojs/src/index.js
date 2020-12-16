ebc9d751716a4afac0dfb4b2987f5e9292f6f37c
steveoni <steohenoni2@gmail.com>
2020-12-16 22:53:02 +0100

    [eslint][s]: lint formatting

--- danfojs/src/index.js
+++ danfojs/src/index.js
@@ -1,10 +1,10 @@
 export { Series } from './core/series';
 export { DataFrame } from './core/frame';
 export { to_datetime } from './core/timeseries';
-export {read_csv, read_json, read_excel, read} from './io/reader'
-export {merge} from './core/merge'
-export {concat} from './core/concat'
-export {LabelEncoder, OneHotEncoder} from './preprocessing/encodings' //TODO Create js file to expose functions
-export {MinMaxScaler, StandardScaler} from './preprocessing/scalers'
-export {date_range} from './core/date_range'
-export {get_dummies} from './core/get_dummies'
+export { read_csv, read_json, read_excel, read } from './io/reader';
+export { merge } from './core/merge';
+export { concat } from './core/concat';
+export { LabelEncoder, OneHotEncoder } from './preprocessing/encodings'; //TODO Create js file to expose functions
+export { MinMaxScaler, StandardScaler } from './preprocessing/scalers';
+export { date_range } from './core/date_range';
+export { get_dummies } from './core/get_dummies';
