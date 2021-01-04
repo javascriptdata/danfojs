107481e90facd225edf386a5192e9f4cf78e2dfa
Rising Odegua <risingodegua@gmail.com>
2021-01-04 14:25:36 +0100

    [types] Update name in typescript declaration file

--- danfojs/types/index.d.ts
+++ danfojs/types/index.d.ts
@@ -9,6 +9,6 @@ import { read_csv, read_json, read_excel, read } from "./io/reader";
 import { LabelEncoder, OneHotEncoder } from "./preprocessing/encodings";
 import { MinMaxScaler, StandardScaler } from "./preprocessing/scalers";
 
-declare module 'danfojs' {
+declare module 'danfojs-node' {
     export { Series, DataFrame, to_datetime, merge, concat, date_range, get_dummies, read_csv, read_json, read_excel, read, LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler }
 }
