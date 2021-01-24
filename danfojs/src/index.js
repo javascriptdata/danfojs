014c39c405438de23f7ed37cb5b167aef78d1d94
Rising Odegua <risingodegua@gmail.com>
2021-01-24 08:37:18 +0100

    Update exported classes

--- danfojs/src/index.js
+++ danfojs/src/index.js
@@ -1,10 +1,16 @@
-export { Series } from './core/series';
-export { DataFrame } from './core/frame';
-export { to_datetime } from './core/timeseries';
-export { read_csv, read_json, read_excel, read } from './io/reader';
-export { merge } from './core/merge';
-export { concat } from './core/concat';
-export { LabelEncoder, OneHotEncoder } from './preprocessing/encodings'; 
-export { MinMaxScaler, StandardScaler } from './preprocessing/scalers';
-export { date_range } from './core/date_range';
-export { get_dummies } from './core/get_dummies';
+import NDframe from "./core/generic";
+
+export { Series } from "./core/series";
+export { DataFrame } from "./core/frame";
+export { to_datetime } from "./core/timeseries";
+export { read_csv, read_json, read_excel, read } from "./io/reader";
+export { merge } from "./core/merge";
+export { concat } from "./core/concat";
+export { LabelEncoder, OneHotEncoder } from "./preprocessing/encodings";
+export { MinMaxScaler, StandardScaler } from "./preprocessing/scalers";
+export { date_range } from "./core/date_range";
+export { get_dummies } from "./core/get_dummies";
+export { Configs } from "./config/config";
+export { NDframe };
+export { Str } from "./core/strings";
+export { Utils } from "./core/utils";
