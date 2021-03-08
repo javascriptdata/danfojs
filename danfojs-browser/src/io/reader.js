3182839608790eb5bf449fc0f0f8beeed3512204
Rising Odegua <risingodegua@gmail.com>
2021-03-08 16:11:03 +0100

    Update read_csv and read_excel to use frictionless

--- danfojs-browser/src/io/reader.js
+++ danfojs-browser/src/io/reader.js
@@ -1,41 +1,43 @@
-import { data } from "@tensorflow/tfjs";
-import XLSX from "xlsx";
-import { Utils } from "../core/utils";
 import { DataFrame } from "../core/frame";
+import { open, Dataset, isDataset } from "frictionless.js";
+import toArray from "stream-to-array";
 
-const utils = new Utils();
 
 /**
  * Reads a CSV file from local or remote storage
  *
- * @param {source} URL or local file path to retreive CSV file. If it's a local path, it
- * must have prefix `file://` and it only works in node environment.
+ * @param {source} URL or local file path to retreive CSV file.
  * @param {config} (Optional). A CSV Config object that contains configurations
  *     for reading and decoding from CSV file(s).
- *
+ *      configs = { size: chunk size of the data to read. This will read from 0 to size of data,
+ *                  data_num (Defaults => 0): The specific dataset to load, when reading data from a datapackage.json,
+ *                  header (Defaults => true): Whether the dataset contains header or not
+ *                }
  * @returns {Promise} DataFrame structure of parsed CSV data
  */
-export const read_csv = async (source, chunk) => {
-  if (
-    !(
-      utils.__is_browser_env() ||
-      source.startsWith("file://") ||
-      source.startsWith("http")
-    )
-  ) {
-    //probabily a relative path, append file:// to it
-    // eslint-disable-next-line no-undef
-    source = `file://${process.cwd()}/${source}`;
-  }
-
-  let tfdata = [];
-  const csvDataset = data.csv(source);
-  const column_names = await csvDataset.columnNames();
-  const sample = csvDataset.take(chunk);
-  await sample.forEachAsync((row) => tfdata.push(Object.values(row)));
-  let df = new DataFrame(tfdata, { columns: column_names });
+export const read_csv = async (source, configs) => {
+  const df = await read(source, configs);
   return df;
 };
+
+/**
+ * Reads an Excel file from local or remote address
+ * @param {source} URL or local file path to retreive excel file.
+ * @param {config} (Optional). A Config object that contains configurations
+ *     for reading and decoding from file(s).
+ *     configs = { sheet: Excel sheet number when reading from excel files
+ *                 size: chunk size of the data to read. This will read from 0 to size of data,
+ *                 data_num (Defaults => 0): The specific dataset to load, when reading data from a datapackage.json,
+ *                 header (Defaults => true): Whether the dataset contains header or not
+ *        }
+ *
+ * @returns {Promise} DataFrame structure of parsed Excel data
+ */
+export const read_excel = async (source, configs) => {
+  let df = await read(source, configs);
+  return df;
+};
+
 
 /**
  * Reads a JSON file from local or remote address
@@ -51,69 +53,52 @@ export const read_json = async (source) => {
 
 };
 
+
 /**
- * Reads an Excel file from local or remote address
- *
- *  * @param {kwargs} kwargs --> {
- *                        source       : string, URL or local file path to retreive Excel file.
- *                        sheet_name   : string, (Optional) Name of the sheet which u want to parse. Default will be the first sheet.
- *                        header_index : int, (Optional) Index of the row which represents the header(columns) of the data. Default will be the first non empty row.
- *                        data_index   : int, (Optional)Index of the row from which actual data(content) starts. Default will be the next row of `header_index`
- *                    }
- * @returns {Promise} DataFrame structure of parsed Excel data
+ * Opens a file using frictionless.js specification.
+ * @param {string} pathOrDescriptor A path to the file/resources. It can be a local file,
+ * a URL to a tabular data (CSV, EXCEL) or Datahub.io Data Resource.
+ * Data comes with extra properties and specification conforming to the Frictionless Data standards.
+ * @param {object} configs { data_num (Defaults => 0): The specific dataset to load, when reading data from a datapackage.json,
+ *                          header (Defaults => true): Whether the dataset contains header or not.
+ *                          }
+ * @returns {DataFrame} Danfo DataFrame/Series
  */
-export const read_excel = async (kwargs) => {
-  let { source, sheet_name, header_index, data_index } = kwargs;
-  let is_a_url = source.match(/(http(s?)):\/\//g);
-  let workbook;
-  if (!header_index) {
-    //default header_index
-    header_index = 1;
-  }
-  if (!data_index) {
-    //default data_index
-    data_index = header_index + 1;
-  }
-  try {
-    let res = await fetch(source, { method: "Get" });
-    res = await res.arrayBuffer();
-    res = new Uint8Array(res);
-    workbook = XLSX.read(res, { type: "array" });
+export const read = async (
+  path_or_descriptor,
+  configs = {}
+) => {
+  let { data_num, header, sheet, size } = configs;
+  data_num = data_num ? data_num : 0;
+  header = header ? header : true;
+  let rows, file;
 
-
-    // Parse worksheet from workbook
-    const worksheet = workbook.Sheets[sheet_name || workbook.SheetNames[0]];
-    let range = XLSX.utils.decode_range(worksheet["!ref"]);
-    let column_names = [],
-      data = [];
-    for (let R = header_index - 1; R <= range.e.r; ++R) {
-      let row_data = [];
-      for (let C = range.s.c; C <= range.e.c; ++C) {
-        let cell_ref;
-
-        //Populate column_names
-        if (R == header_index - 1) {
-          cell_ref = XLSX.utils.encode_cell({ c: C, r: header_index - 1 });
-          if (worksheet[cell_ref]) {
-            column_names.push(worksheet[cell_ref].v);
-          }
-        }
-
-        //Populate corresponding data row
-        if (R >= data_index - 1) {
-          cell_ref = XLSX.utils.encode_cell({ c: C, r: R });
-          if (worksheet[cell_ref]) {
-            row_data.push(worksheet[cell_ref].v);
-          }
-        }
-      }
-      if (R >= data_index - 1) {
-        data.push(row_data);
-      }
+  if (isDataset(path_or_descriptor)) {
+    console.log(
+      "datapackage.json found. Loading Dataset package from Datahub.io"
+    );
+    const dataset = await Dataset.load(path_or_descriptor);
+    file = dataset.resources[data_num];
+    rows = await toArray(await file.rows({ sheet, size }));
+  } else {
+    try {
+      file = open(path_or_descriptor, {});
+      rows = await toArray(await file.rows({ sheet, size }));
+    } catch (error) {
+      console.log(error);
     }
-    let df = new DataFrame(data, { columns: column_names });
+  }
+
+  if ([ "csv", "xls", "xlsx" ].includes(await file.descriptor.format)) {
+    if (header) {
+      let df = new DataFrame(rows.slice(1), { columns: rows[0] });
+      return df;
+    } else {
+      let df = new DataFrame(rows);
+      return df;
+    }
+  } else {
+    let df = new DataFrame(rows);
     return df;
-  } catch (err) {
-    throw new Error(err);
   }
 };
--- danfojs-browser/tests/io/reader.js
+++ danfojs-browser/tests/io/reader.js
@@ -5,10 +5,9 @@ describe("read_csv", async function () {
     const csvUrl =
       "https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv";
 
-    dfd.read_csv(csvUrl).then((df) => {
-      const num_of_columns = df.column_names.length;
-      assert.equal(num_of_columns, 13);
-    });
+    const df = await dfd.read_csv(csvUrl);
+    const num_of_columns = df.column_names.length;
+    assert.equal(num_of_columns, 13);
   });
 
   it("reads a csv file from source from local disk", async function () {
@@ -48,14 +47,14 @@ describe("read_excel", async function () {
   it("reads an excel file from source over the internet", async function () {
     const remote_url =
       "https://file-examples-com.github.io/uploads/2017/02/file_example_XLS_100.xls";
-    dfd.read_excel({ source: remote_url }).then((df) => {
+    dfd.read_excel(remote_url).then((df) => {
       assert(df.columns.length, 8);
     });
   });
 
   it("reads an excel file from source from local disk", async function () {
     const file_url = "danfojs/tests/samples/SampleData.xlsx";
-    dfd.read_excel({ source: file_url, header_index: 7, data_index: 8 }).then(
+    dfd.read_excel(file_url).then(
       (df) => {
         assert(df.columns.length, 4);
       }
