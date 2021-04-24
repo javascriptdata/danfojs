a54f618b804fe81c2947f73abf6fa37c2d408321
steveoni <steohenoni2@gmail.com>
2021-04-24 02:21:02 +0000

    [frame.js][s]:  obtain groupby column dtype to fix #183

--- danfojs-node/src/core/frame.js
+++ danfojs-node/src/core/frame.js
@@ -1110,7 +1110,10 @@ export class DataFrame extends Ndframe {
     let column_names = this.column_names;
     let col_dict = {};
     let key_column = null;
-
+    let col_index = col.map((val) => column_names.indexOf(val));
+    let col_dtype = this.dtypes.filter((val, index) => {
+      return col_index.includes(index);
+    });
     if (col.length == 2) {
       if (column_names.includes(col[0])) {
         // eslint-disable-next-line no-unused-vars
@@ -1172,7 +1175,8 @@ export class DataFrame extends Ndframe {
       col_dict,
       key_column,
       this.values,
-      column_names
+      column_names,
+      col_dtype
     ).group();
 
     return groups;
