0dcb99d1b79865c47914604d32af9f6de1bb86f6
Rising Odegua <risingodegua@gmail.com>
2021-06-02 09:05:17 +0100

    [Patch] Remove read export from danfo-browser

--- danfojs-browser/src/index.js
+++ danfojs-browser/src/index.js
@@ -4,7 +4,7 @@ import * as tf from "@tensorflow/tfjs";
 export { Series } from "./core/series";
 export { DataFrame } from "./core/frame";
 export { to_datetime } from "./core/timeseries";
-export { read_csv, read_json, read_excel, read } from "./io/reader";
+export { read_csv, read_json, read_excel } from "./io/reader";
 export { merge } from "./core/merge";
 export { concat } from "./core/concat";
 export { LabelEncoder, OneHotEncoder } from "./preprocessing/encodings";
