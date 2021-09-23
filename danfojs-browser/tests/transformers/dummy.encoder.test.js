0d612e9aebb3a0ca0be755e8824313a06bfe931a
Rising Odegua <risingodegua@gmail.com>
2021-09-23 11:40:10 +0100

    Add test for encoders and scalers

---
+++ danfojs-browser/tests/transformers/dummy.encoder.test.js
@@ -0,0 +1,137 @@
+/* eslint-disable no-undef */
+
+describe("DummyEncoder", function () {
+  it("getDummies works on Series", function () {
+
+    const data = [ "dog", "male", "female", "male", "female", "male", "dog" ];
+    const series = new dfd.Series(data);
+    const df = dfd.getDummies(series, { prefix: "test", prefixSeparator: "/" });
+
+    const dfValues = [
+      [ 1, 0, 0 ],
+      [ 0, 1, 0 ],
+      [ 0, 0, 1 ],
+      [ 0, 1, 0 ],
+      [ 0, 0, 1 ],
+      [ 0, 1, 0 ],
+      [ 1, 0, 0 ]
+    ];
+    const dfColumns = [ 'test/dog', 'test/male', 'test/female' ];
+    assert.deepEqual(df.values, dfValues);
+    assert.deepEqual(df.columns, dfColumns);
+  });
+  it("getDummies works on Series with default prefix and prefixSeperator", function () {
+
+    const data = [ "dog", "male", "female", "male", "female", "male", "dog" ];
+    const series = new dfd.Series(data);
+    const df = dfd.getDummies(series);
+
+    const dfValues = [
+      [ 1, 0, 0 ],
+      [ 0, 1, 0 ],
+      [ 0, 0, 1 ],
+      [ 0, 1, 0 ],
+      [ 0, 0, 1 ],
+      [ 0, 1, 0 ],
+      [ 1, 0, 0 ]
+    ];
+    const dfColumns = [ '0_dog', '1_male', '2_female' ];
+    assert.deepEqual(df.values, dfValues);
+    assert.deepEqual(df.columns, dfColumns);
+  });
+
+  it("getDummies works on DataFrame", function () {
+
+    const data = [ [ 1, "dog", 1.0, "fat" ], [ 3, "fog", 2.0, "good" ], [ 4, "gof", 3.0, "best" ] ];
+    const columns = [ "A", "B", "C", "d" ];
+    const df = new dfd.DataFrame(data, { columns: columns });
+
+    const df1 = dfd.getDummies(df, { prefixSeparator: [ "_", "#" ], columns: [ "A", "d" ], prefix: "test" });
+    const expectedColumns = [ 'B', 'C', 'test_1', 'test_3', 'test_4', 'test#fat', 'test#good', 'test#best' ];
+    const expected = [ [ 'dog', 1.0, 1, 0, 0, 1, 0, 0 ],
+      [ 'fog', 2.0, 0, 1, 0, 0, 1, 0 ],
+      [ 'gof', 3.0, 0, 0, 1, 0, 0, 1 ] ];
+    assert.deepEqual(df1.values, expected);
+    assert.deepEqual(df1.columns, expectedColumns);
+
+  });
+  it("Throw error if the prefix specified is not equal to the column specified", function () {
+
+    const data = [ [ 1, "dog", 1.0, "fat" ], [ 3, "fog", 2.0, "good" ], [ 4, "gof", 3.0, "best" ] ];
+    const columns = [ "A", "B", "C", "d" ];
+    const df = new dfd.DataFrame(data, { columns: columns });
+
+    assert.throws(function () { dfd.getDummies(df, { prefix: [ "fg" ], prefixSeparator: "_", columns: [ "A", "d" ] }); }, Error,
+      `ParamError: prefix and data array must be of the same length. If you need to use the same prefix, then pass a string param instead. e.g {prefix: "fg"}`);
+
+  });
+  it("replace column sepecified with prefix", function () {
+
+    const data = [ [ 1, "dog", 1.0, "fat" ], [ 3, "fog", 2.0, "good" ], [ 4, "gof", 3.0, "best" ] ];
+    const columns = [ "A", "B", "C", "d" ];
+    const df = new dfd.DataFrame(data, { columns: columns });
+
+    const df1 = dfd.getDummies(df, { prefix: [ "F", "G" ], prefixSeparator: "_", columns: [ "A", "d" ] });
+    const expectedColumns = [
+      'B', 'C',
+      'F_1', 'F_3',
+      'F_4', 'G_fat',
+      'G_good', 'G_best'
+    ];
+
+    const expected = [ [ 'dog', 1.0, 1, 0, 0, 1, 0, 0 ],
+      [ 'fog', 2.0, 0, 1, 0, 0, 1, 0 ],
+      [ 'gof', 3.0, 0, 0, 1, 0, 0, 1 ] ];
+
+    assert.deepEqual(df1.values, expected);
+    assert.deepEqual(df1.columns, expectedColumns);
+
+  });
+
+  it("getDummies auto infers and encode columns with string dtype", function () {
+
+    const data = [ [ 1, "dog", 1.0, "fat" ], [ 3, "fog", 2.0, "good" ], [ 4, "gof", 3.0, "best" ] ];
+    const columns = [ "A", "B", "C", "d" ];
+    const df = new dfd.DataFrame(data, { columns: columns });
+
+    const df1 = dfd.getDummies(df, { prefixSeparator: "_" });
+    const expectedColumns = [
+      'A', 'C',
+      'B_dog', 'B_fog',
+      'B_gof', 'd_fat',
+      'd_good', 'd_best'
+    ];
+    const expected = [
+      [
+        1, 1, 1, 0,
+        0, 1, 0, 0
+      ],
+      [
+        3, 2, 0, 1,
+        0, 0, 1, 0
+      ],
+      [
+        4, 3, 0, 0,
+        1, 0, 0, 1
+      ]
+    ];
+    assert.deepEqual(df1.values, expected);
+    assert.deepEqual(df1.columns, expectedColumns);
+
+  });
+
+  it("should one hot encode all other columns", function () {
+
+    const data = [ [ 1, "dog", 1.0, "fat" ], [ 3, "fog", 2.0, "good" ], [ 4, "gof", 3.0, "best" ] ];
+    const columns = [ "A", "B", "C", "d" ];
+    const df = new dfd.DataFrame(data, { columns: columns });
+    const rslt = [
+      [ 1, 'dog', 1, 1, 0, 0 ],
+      [ 3, 'fog', 2, 0, 1, 0 ],
+      [ 4, 'gof', 3, 0, 0, 1 ]
+    ];
+
+    assert.deepEqual(dfd.getDummies(df, { columns: [ "d" ] }).values, rslt);
+
+  });
+});
---
+++ danfojs-browser/tests/transformers/min.max.scaler.test.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+
+describe("MinMaxscaler", function () {
+
+  it("Standardize values in a DataFrame using a MinMaxScaler", function () {
+    const data = [ [ -1, 2 ], [ -0.5, 6 ], [ 0, 10 ], [ 1, 18 ] ];
+    const scaler = new dfd.MinMaxScaler();
+
+    const expected = [ [ 0, 0 ], [ 0.25, 0.25 ], [ 0.5, 0.5 ], [ 1, 1 ] ];
+    const transformedData = [ [ 1.5, 0. ] ];
+
+    scaler.fit(new dfd.DataFrame(data));
+    const resultDf = scaler.transform(new dfd.DataFrame(data));
+    assert.deepEqual(resultDf.values, expected);
+    assert.deepEqual(scaler.transform([ [ 2, 2 ] ]), transformedData);
+  });
+  it("fitTransform using a MinMaxScaler", function () {
+    const data = [ [ -1, 2 ], [ -0.5, 6 ], [ 0, 10 ], [ 1, 18 ] ];
+    const scaler = new dfd.MinMaxScaler();
+    const resultDf = scaler.fitTransform(new dfd.DataFrame(data));
+
+    const expected = [ [ 0, 0 ], [ 0.25, 0.25 ], [ 0.5, 0.5 ], [ 1, 1 ] ];
+    assert.deepEqual(resultDf.values, expected);
+  });
+  it("InverseTransform with MinMaxScaler", function () {
+    const scaler = new dfd.MinMaxScaler();
+    scaler.fit([ 1, 2, 3, 4, 5 ]);
+    const resultTransform = scaler.transform([ 1, 2, 3, 4, 5 ]);
+    const resultInverse = scaler.inverseTransform([ 0, 0.25, 0.5, 0.75, 1 ]);
+
+    assert.deepEqual(resultTransform, [ 0, 0.25, 0.5, 0.75, 1 ]);
+    assert.deepEqual([ 1, 2, 3, 4, 5 ], resultInverse);
+  });
+  it("Index and columns are kept after transformation", function () {
+    const data = [ [ -1, 2 ], [ -0.5, 6 ], [ 0, 10 ], [ 1, 18 ] ];
+    const df = new dfd.DataFrame(data, { index: [ 1, 2, 3, 4 ], columns: [ "a", "b" ] });
+
+    const scaler = new dfd.MinMaxScaler();
+    scaler.fit(df);
+    const resultDf = scaler.transform(df);
+
+    assert.deepEqual(resultDf.index, [ 1, 2, 3, 4 ]);
+    assert.deepEqual(resultDf.columns, [ "a", "b" ]);
+  });
+  it("Standardize values in a Series using a MinMaxScaler", function () {
+    const data = [ -1, 2, -0.5, 60, 101, 18 ];
+    const scaler = new dfd.MinMaxScaler();
+    const result = [ 0, 0.029411764815449715, 0.0049019609577953815, 0.5980392098426819, 1, 0.18627451360225677 ];
+    const transformedData = [ 0.029411764815449715, 0.029411764815449715 ];
+    scaler.fit(new dfd.Series(data));
+    assert.deepEqual(scaler.transform(new dfd.Series(data)).values, result);
+    assert.deepEqual(scaler.transform([ 2, 2 ]), transformedData);
+  });
+});
