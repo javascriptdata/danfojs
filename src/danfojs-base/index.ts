86e43a29a12df881bda82a8a8e7149a9e957f830
Rising Odegua <risingodegua@gmail.com>
2022-04-02 13:52:24 +0100

    Update version to latest

--- src/danfojs-base/index.ts
+++ src/danfojs-base/index.ts
@@ -29,7 +29,7 @@ import merge from "./transformers/merge"
 import dateRange from "./core/daterange"
 import tensorflow from "./shared/tensorflowlib"
 
-const __version = "1.0.4";
+const __version = "1.0.5";
 
 export {
     NDframe,
